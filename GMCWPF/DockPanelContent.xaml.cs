38c229f041274177c3533fe5381cddfbed355210
volante1013 <u.swb01140@gmail.com>
2017-12-09 01:57:59 +0900

    工数内容入力用のUserControlを追加

---
+++ GMCWPF/DockPanelContent.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GMCWPF
+{
+	/// <summary>
+	/// DockPanelContent.xaml の相互作用ロジック
+	/// </summary>
+	public partial class DockPanelContent : UserControl
+	{
+		public DockPanelContent ()
+		{
+			InitializeComponent();
+		}
+
+		public Button GetPlusBtn { get{ return PlusBtn_Content; } }
+		public Button GetMinusBtn { get { return MinusBtn_Content; } }
+
+		public void setBindContentBox (Manhours manhours, int index)
+		{
+			var binding = new Binding(string.Format($"[{index}]")) { Source = manhours };
+			ContentBox.SetBinding(TextBox.TextProperty, binding);
+		}
+	}
+}
